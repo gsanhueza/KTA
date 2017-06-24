b1e4e3bb868e4429843ed80c464326add74048e4
juakotorres <juako.torres@hotmail.com>
2017-06-24 18:29:36 -0400

    Agregar git ignore y proyecto base

---
+++ KTA/Source/KTA/KTAGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "KTAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KTA_API AKTAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
